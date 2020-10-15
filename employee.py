07b281581d607ae44a5185578ffd73671279398b
FuryWingsYT <32018052+FuryWingsYT@users.noreply.github.com>
2020-10-15 15:13:42 -0400

    Update employee.py

--- employee.py
+++ employee.py
@@ -33,6 +33,27 @@ class OrderTaker(Employee):
         order.order_taken = True
         self.orders_taken += 1
 
+class SandwichMaker(Employee):
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.sandwiches_made = 0
+    def __str__(self):
+        return super().__str__() + "\n Sandwiches made: " + str(self.sandwiches_made)
+    def check_out(self, order):
+        order.sandwich_made= True
+        self.sandwiches_made += 1
+
+class Cashier(Employee):
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.payments_taken = 0
+    def __str__(self):
+        return super().__str__() + "\n Customers checked out: " + str(self.payments_taken)
+    def check_out(self, order):
+        order.checked_out= True
+        self.payments_taken += 1
 # Create SandwichMaker and Cashier classes which inherit from Employee.
 
 def done(orders):
@@ -46,9 +67,13 @@ def main():
     orders  = [Order(n) for n in range(50)] # This line creates a list of orders
 
     joe = OrderTaker("Joe")
-
+    jim = SandwichMaker("Jim")
+    steve = Cashier("Steve")
     # Using an OrderTaker, a SandwichMaker, and a Cashier, complete all the orders. 
-
+    for i in orders:
+        joe.check_out(i)
+        jim.check_out(i)
+        steve.check_out(i)
     done(orders)
     
 
